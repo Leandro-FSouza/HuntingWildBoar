fdb9e47fde13d6d39a1f2311d0c535387fe5c417
Leandro-FSouza <leandro.fs.mcz@hotmail.com>
2023-02-17 23:13:35 -0300

    Creating package Classes and class General

---
+++ src/Classes/General.java
@@ -0,0 +1,76 @@
+package Classes;
+
+import java.util.Random;
+
+public class General {
+    private String name;
+    private Integer strength, hp, level, mp, dexterity, damage;
+
+    public void attack() {
+        Random random = new Random();
+        int d20 = random.nextInt(20);
+        d20++;
+        if (d20 == 1) {
+            System.out.printf("Fail! You Missed!");
+        } else {
+            damage = strength;
+            System.out.println("You hit the enemy!");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public void setStrength(Integer strength) {
+        this.strength = strength;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getMp() {
+        return mp;
+    }
+
+    public void setMp(Integer mp) {
+        this.mp = mp;
+    }
+
+    public Integer getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(Integer dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public Integer getDamage() {
+        return damage;
+    }
+
+    public void setDamage(Integer damage) {
+        this.damage = damage;
+    }
+}
