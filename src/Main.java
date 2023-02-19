c06569cf6d1f1acc0b3edf2515800ce7c87792e4
Leandro-FSouza <leandro.fs.mcz@hotmail.com>
2023-02-18 23:41:42 -0300

    Creating package Game and class Prologue.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import Bestiary.*;
+import Classes.*;
+import Game.*;
+
 public class Main {
     public static void main(String[] args) {
-
-
+        Prologue mainCharacter = new Prologue();
+        mainCharacter.startGame();
     }
 }
